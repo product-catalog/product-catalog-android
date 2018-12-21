62262ddd6fb3b887df2bd0fbe63f5504896e817e
siqqQ <b.bohosyan@gmail.com>
2018-12-21 18:25:09 +0200

    Deleted unused activity xml and changed welcomeactivity id xml

--- app/src/main/java/com/pcatalog/productcatalog/views/WelcomeActivity.java
+++ app/src/main/java/com/pcatalog/productcatalog/views/WelcomeActivity.java
@@ -10,6 +10,6 @@ public class WelcomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome2);
+        setContentView(R.layout.activity_welcome);
     }
 }
